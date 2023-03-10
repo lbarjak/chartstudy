30f9f8bec23fd4b77b954de21876e683dfebabbf
lbarjak <barjak.laszlo@gmail.com>
2023-03-10 21:03:15 +0100

    transformations

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 console.log(temps)
 
-class Graph {
-    constructor() {
-        this.graph = document.getElementsByTagName('canvas')[0]
+let graph = document.getElementsByTagName('canvas')[0]
+
+function drawLine(ctx, begin, end, stroke = 'black', width = 1) {
+    if (stroke) {
+        ctx.strokeStyle = stroke;
     }
-    draw() {
-        window.ctx = this.graph.getContext('2d')
-        this.graph.width = window.innerWidth
-        this.graph.height = window.innerHeight
-        ctx.fillStyle = '#4d4d4d'
-        ctx.fillRect(0, 0, this.graph.width, this.graph.height)
+
+    if (width) {
+        ctx.lineWidth = width;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(...begin);
+    ctx.lineTo(...end);
+    ctx.stroke();
+}
+
+window.ctx = graph.getContext('2d')
+graph.width = window.innerWidth * 0.95
+graph.height = window.innerHeight * 0.9
+let width = graph.width
+let height = graph.height
+let xMargin = width * 0.01
+let yMargin = height * 0.01
+ctx.fillStyle = 'lightgray'
+ctx.fillRect(0, 0, width, height)
+ctx.transform(1, 0, 0, -1, 0, height)
+ctx.translate(width * 0.002, height * 0.005)
+drawLine(ctx, [xMargin, yMargin], [width * 0.995 - xMargin, yMargin])
+drawLine(ctx, [xMargin, yMargin], [xMargin, height * 0.99 - yMargin])
+
+xTicks = () => {
+    for (i = 1; i <= 10; i++) {
+        let x = xMargin + 0.95 * width * i / 10
+        drawLine(ctx, [x, 0], [x, 20])
     }
 }
-var graph = new Graph()
-graph.draw()
+
+xTicks()
+
+
+
+
 
