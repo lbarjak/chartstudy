897dcd7f8b21b9832dfba44bab678f6bd32bfe30
lbarjak <barjak.laszlo@gmail.com>
2023-03-12 22:28:22 +0100

    xStickDates added

--- dates.js
+++ dates.js
@@ -1,25 +1,30 @@
 let date1 = new Date("2023-03-10")
 let date2 = new Date("2023-03-20")
 let diff = date2 - date1
-let newDate
+let xStickDates = []
+
 const options1 = {
     year: "numeric",
     month: "numeric",
     day: "numeric",
 };
 const options2 = {
-    weekday: "long"
-};
-const options3 = {
     hour: "2-digit",
     minute: "2-digit"
 }
+const options3 = {
+    weekday: "long"
+};
+
 dates = () => {
     console.log(diff)
     for (i = date1.getTime() - 3600000; i <= date2.getTime(); i += 21600000) {
         newDate = new Date(i)
-        console.log(newDate.toLocaleString('hu-HU', options1));
-        console.log(newDate.toLocaleString('hu-HU', options2));
-        console.log(newDate.toLocaleTimeString('hu-HU', options3));
+        xStickDates.push([
+            newDate.toLocaleString('hu-HU', options1).replaceAll(' ', ''),
+            newDate.toLocaleTimeString('hu-HU', options2),
+            newDate.toLocaleString('hu-HU', options3)
+        ])
     }
+    console.log(xStickDates)
 }
