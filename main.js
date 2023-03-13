800625ace2616d6acbf6ecf75e29d6128de9d3db
lbarjak <barjak.laszlo@gmail.com>
2023-03-13 21:58:53 +0100

    Inconsolata condensed font added

--- main.js
+++ main.js
@@ -25,7 +25,8 @@ let height = graph.height
 ctx.fillStyle = 'lightgray'
 ctx.fillRect(0, 0, width, height)
 let fontSize = 18
-ctx.font = fontSize + "px sans"
+ctx.font = fontSize + "px 'Inconsolata'"
+ctx.fontStretch = "condensed";
 ctx.textAlign = "end"
 
 ctx.transform(0.9, 0, 0, -0.8, width * 0.07, height * 0.85)
