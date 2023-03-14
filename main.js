fcf5e34f94f56f232da3617e0b85189d1c6c5a67
lbarjak <barjak.laszlo@gmail.com>
2023-03-14 14:42:05 +0100

    adjusted to 1366x768

--- main.js
+++ main.js
@@ -24,12 +24,13 @@ let height = graph.height
 
 ctx.fillStyle = 'lightgray'
 ctx.fillRect(0, 0, width, height)
-let fontSize = 18
+let fontSize = Math.round(height / 40)
+console.log(fontSize)
 ctx.font = fontSize + "px 'Inconsolata'"
 ctx.fontStretch = "condensed";
 ctx.textAlign = "end"
 
-ctx.transform(0.9, 0, 0, -0.8, width * 0.07, height * 0.85)
+ctx.transform(0.9, 0, 0, -0.8, width * 0.07, height * 0.83)
 
 drawLine(ctx, [0, 0], [width, 0])
 drawLine(ctx, [0, 0], [0, height])
